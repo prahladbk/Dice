43affe113b30a88c78b368e6b6fe18f647e2c166
Prahlad BK <81477136+prahladbk@users.noreply.github.com>
2022-05-03 20:57:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+var n1=Math.round(Math.random()*5)+1;
+var n2=Math.round(Math.random()*5)+1;
+if(n1==1){
+    document.getElementById('img1').src="images/dice1.png";
+}
+else if(n1==2){
+    document.getElementById('img1').src="images/dice2.png";
+}
+else if(n1==3){
+    document.getElementById('img1').src="images/dice3.png";
+}
+else if(n1==4){
+    document.getElementById('img1').src="images/dice4.png";
+}
+else if(n1==5){
+    document.getElementById('img1').src="images/dice5.png";
+}
+else if(n1==6){
+    document.getElementById('img1').src="images/dice6.png";
+}
+else{
+    console.log("Error");
+}
+if(n2==1){
+    document.getElementById('img2').src="images/dice1.png";
+}
+else if(n2==2){
+    document.getElementById('img2').src="images/dice2.png";
+}
+else if(n2==3){
+    document.getElementById('img2').src="images/dice3.png";
+}
+else if(n2==4){
+    document.getElementById('img2').src="images/dice4.png";
+}
+else if(n2==5){
+    document.getElementById('img2').src="images/dice5.png";
+}
+else if(n2==6){
+    document.getElementById('img2').src="images/dice6.png";
+}
+else{
+    console.log("Error");
+}
